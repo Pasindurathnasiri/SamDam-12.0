74af8d7d024cadff919f077c21981f459514a601
Pasindu Madhuranga <45681668+Pasindurathnasiri@users.noreply.github.com>
2020-08-21 18:06:55 +0530

    first chart successfully implemented

--- Samdam 12.0/Sam Dam 12.0/Angular 6/src/app/eb-monthly-reports/eb-monthly-reports.component.ts
+++ Samdam 12.0/Sam Dam 12.0/Angular 6/src/app/eb-monthly-reports/eb-monthly-reports.component.ts
@@ -1,8 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormBuilder, FormGroup ,Validators, NgForm } from '@angular/forms';
 import { MaterialService } from '../shared/material.service';
 import { MatTableDataSource } from '@angular/material/table';
+import { SiteService } from '../shared/site.service';
 import { Eqipment } from '../shared/equipment.model';
 
+interface Site{
+  _id:string;
+  site_id:string;
+  name: string;
+  site: Array<Site>;
+}
 
  
 @Component({
@@ -11,39 +19,77 @@ import { Eqipment } from '../shared/equipment.model';
   styleUrls: ['./eb-monthly-reports.component.css']
 })
 export class EbMonthlyReportsComponent implements OnInit {
+  siteControl = new FormControl('',Validators.required);
+  siteFormControl = new FormControl('', Validators.required);
+  sites: Site [] = [];
+  AllSiteDate:any=[];
   AllEquipmentData: any =[];
+  chartdata: any=[];
   public dataSourceEQ: MatTableDataSource<Eqipment>;
  
-  title = 'Working Progress of Construction Sites,2020';
-   type = 'BarChart';
-  data = [
-    ['Firefox', 45.0],
-    ['IE', 26.8],
-    ['Chrome', 12.8],
-    ['Safari', 8.5],
-    ['Opera', 6.2],
-    ['Others', 0.7] 
- ];
- columnNames = ['Site', 'Progress'];
+  title = 'Available Stocks in Warehouse,2020';
+  title2='';
+  type = 'BarChart';
+  type2='BarChart';
+  columnNames2 = ['Equipment', 'Amount'];
+ 
+ columnNames = ['Equipment', 'Amount'];
+ options2 = {
+  colors: ['#e0440e', '#e6693e', '#ec8f6e', '#f3b49f', '#f6c7b6'], is3D: true
+};
  options = {
   colors: ['#e0440e', '#e6693e', '#ec8f6e', '#f3b49f', '#f6c7b6'], is3D: true
 };
+   width2 = 850;
    width = 850;
    height = 700;
-  constructor(private materialService:MaterialService) {
+   height2 = 700;
+  constructor(private materialService:MaterialService,private siteService:SiteService) {
     this.materialService.getAllEquipment().subscribe(dataEQ=>{
      this.AllEquipmentData=dataEQ;
+     console.log(this.AllEquipmentData);
      
-     this.dataSourceEQ=new MatTableDataSource<Eqipment>(this.AllEquipmentData);
-     console.log(this.AllEquipmentData.length)
     })
-    console.log(this.data);
-   
+
+    //get sites
+    this.siteService.GetAllSites().subscribe(data =>{
+      this.AllSiteDate = data;
+      //in here pass sites to mat-select
+       setTimeout(()=>{
+  
+       },0)
+       this.sites = this.AllSiteDate;
+    })
+    
     
    }
 
   ngOnInit(): void {
     
+  }
+
+  getData(){
+    var f_index;
+    var s_index;
+    for(var i=0;i<this.AllEquipmentData.length;i++){
+      f_index=this.AllEquipmentData[i].eq_type[0].eq_type;
+      s_index=this.AllEquipmentData[i].amount;
+      
+      this.chartdata[i] =[`${f_index}`,s_index];
+    }
+    return this.chartdata;
+  }
+
+  getData2(){
+    var f_index;
+    var s_index;
+    for(var i=0;i<this.AllEquipmentData.length;i++){
+      f_index=this.AllEquipmentData[i].eq_type[0].eq_type;
+      s_index=this.AllEquipmentData[i].amount;
+      
+      this.chartdata[i] =[`${f_index}`,s_index];
+    }
+    //return this.chartdata;
   }
 
 }
